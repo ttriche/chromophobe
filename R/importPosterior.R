f2b1e09ab8b86cf6b2423563b5f680a928c7453a
Tim Triche, Jr <ttriche@usc.edu>
2012-10-14 15:12:09 -0700

    added rJava hooks, import.posterior for enrichment

---
+++ R/importPosterior.R
@@ -0,0 +1,12 @@
+import.posterior <- function(pathOrHMM, states=NULL) {
+
+  ## import posterior probabilities into a sparse Matrix 
+  ##
+  ## if given a path, troll through chrom by chrom to get a GRanges of matrices
+  ## if given an HMM GRanges/GRangesList, turn it into a SummarizedExperiment
+  ## 
+
+  stop("Ain't done yet.  Feel free to send me a patch though! --t")
+
+}
+
