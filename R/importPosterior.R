dc904a5f3703b9b41c984d462e38ffd476d069e9
Tim Triche, Jr <ttriche@usc.edu>
2012-10-14 17:35:46 -0700

    changes for 0.4 prep: add plot command, posteriors

--- R/importPosterior.R
+++ R/importPosterior.R
@@ -1,11 +1,15 @@
-import.posterior <- function(pathOrHMM, states=NULL) {
+import.posterior <- function(HMM, posteriorPath, states=NULL) {
 
-  ## import posterior probabilities into a sparse Matrix 
+  ## given an HMM GRanges/GRangesList, turn it into a SummarizedExperiment, by
+  ## loading the posterior probabilities into a sparse Matrix for each GRanges
+  ## (so if the HMMs are in a GRangesList, the result will be a list of SEs)
   ##
-  ## if given a path, troll through chrom by chrom to get a GRanges of matrices
-  ## if given an HMM GRanges/GRangesList, turn it into a SummarizedExperiment
+  ## FIXME: make sure to distinguish a list of GRanges from a GRangesList, 
+  ##        in the sense that a GRangesList with full genome coverage is a very
+  ##        different animal from a per-state GRangesList that can be requested
+  ##        from import.ChromHMM for producing pyro plots.  The latter must be 
+  ##        unlisted back into a single GRanges in order to cough up an HMM SE.
   ## 
-
   stop("Ain't done yet.  Feel free to send me a patch though! --t")
 
 }
---
+++ R/plot.ChromHMM.R
@@ -0,0 +1,9 @@
+## plot genome % for a given HMM's states
+##
+## soon: plot a bunch of other shit too
+plot.ChromHMM <- function(HMM, what='genome') {
+
+  stop("Ain't done yet")
+
+}
+
