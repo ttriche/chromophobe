59e9abebead4f00e2e77de4ca18528f1659b8161
Tim Triche, Jr <ttriche@usc.edu>
2012-10-14 17:40:07 -0700

    minor changes and TODOs for plotting

--- R/plot.ChromHMM.R
+++ R/plot.ChromHMM.R
@@ -3,6 +3,15 @@
 ## soon: plot a bunch of other shit too
 plot.ChromHMM <- function(HMM, what='genome') {
 
+  require(ggplot2)
+
+  ## initially: do a stack plot of genomic state occupancy and co-occurrence 
+  ##            for a list of HMMs (e.g. the RoadCODE3 runs, for example) to 
+  ##            allow coalescing and pruning redundant states.  
+  ## 
+  ## later:     CTCF enrichments, pyroPlots, etc.
+  ## 
+
   stop("Ain't done yet")
 
 }
