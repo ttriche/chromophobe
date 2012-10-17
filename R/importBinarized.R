495a68f6eebd7de1719fcf5621191cfaf05e4c52
Tim Triche, Jr <ttriche@usc.edu>
2012-10-17 07:13:45 -0700

    start import.binarized for mark enrichment plots

---
+++ R/importBinarized.R
@@ -0,0 +1,12 @@
+import.binarized <- function(HMM, binarizedPath) {
+
+  ## given an HMM GRanges/GRangesList, read in the matrices of marks (0/1/NA)
+  ## corresponding to the binned/binarized data from ChromHMM BinarizeBed and
+  ## stuff them into sparse Matrix objects in a SummarizedExperiment
+  ##
+  ## This may require some serious trickery in order to be doable properly in R
+  ## 
+  stop("Ain't done yet.  Feel free to send me a patch though! --t")
+
+}
+
