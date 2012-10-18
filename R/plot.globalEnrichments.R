1ca21f0a75b17868f930d2582e53148d36f63a63
Tim Triche, Jr <ttriche@usc.edu>
2012-10-18 13:50:17 -0700

    beginnings of plots via TransView and pheatmap

---
+++ R/plot.globalEnrichments.R
@@ -0,0 +1,7 @@
+plot.globalEnrichments <- function(HMM) {
+
+  # genome-wide enrichments via ggplot2 and pheatmap
+
+  message('not finished yet')
+
+} 
---
+++ R/plot.localEnrichments.R
@@ -0,0 +1,8 @@
+plot.localEnrichments <- function(HMM) {
+
+  require(TransView)
+  # CTCF, etc. via TransView
+
+  message('Not finished yet')
+
+}
