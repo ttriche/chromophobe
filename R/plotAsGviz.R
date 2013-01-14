8ed36a0e71ca9015569c67f3cf74e1ecd02720ed
Tim Triche, Jr <ttriche@usc.edu>
2013-01-14 12:54:54 -0800

    various updates, some for Toshi

---
+++ R/plotAsGviz.R
@@ -0,0 +1,9 @@
+## plot one or more 
+##
+## FIXME: handle WIG tracks and other things with scores/depths, like segtools!
+##
+plotAsGviz <- function(object, GR, colors=NULL, ...) {
+  require(Gviz)
+  ## see https://stat.ethz.ch/pipermail/bioconductor/2013-January/050247.html
+  stop('Plotting JointSegmentations in Gviz is almost, but not quite, done')
+}
---
+++ R/plotGenometriCorr.R
@@ -0,0 +1,9 @@
+## plot genometric correlations for a specific region and metric
+## will probably wrap plotAsGviz in the very near future...
+##
+## FIXME: handle WIG tracks and other things with scores/depths
+##
+plotGenometriCorr <- function(object, GR, colors=NULL, ...) {
+  require(GenometriCorr)
+  stop('Plotting of genometric correlations is not finished yet')
+}
