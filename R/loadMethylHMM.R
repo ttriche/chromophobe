855dc12da07993bbd546a7b2079e5983ee3f76dd
Tim Triche, Jr <ttriche@usc.edu>
2013-05-31 10:44:01 -0700

    added loadMethylHMM fn, parallel loading

--- R/loadMethylHMM.R
+++ R/loadMethylHMM.R
@@ -1,21 +1,13 @@
-loadChromHMM <- function(path='.',genome=NULL,states=NULL,files=NULL,para=T) { 
+## realistically, if trans/emis can't be estimated, we've got a problem...
+loadMethylHMM <- function(path='.',genome=NULL,states=NULL,files=NULL,para=T) { 
 
   require(rtracklayer)
   if(path != '.') oldwd <- getwd()
   setwd(path)
   argv <- list()
-  if(is.null(files)) {
-    files <- list.files(patt='.*_segments.bed$')
-    names(files) <- gsub('_segments.bed$', '', files)
-  }
+  if(is.null(files)) files <- list.files(patt='.*ethylHMM.*bed$')
   if(is.null(names(files))) names(files) <- files
 
-  ## use the color-coded, labeled files if present...
-  #
-  # densefiles <- sapply(files, function(x) 
-  #                     list.files(patt=gsub('_segments','_dense',x)))
-  # if(length(densefiles) == length(files)) files <- densefiles
-  #
   buildStates <- TRUE
   if(is.null(states)) {
     if('labelStates.txt' %in% list.files()) {
@@ -29,26 +21,8 @@ loadChromHMM <- function(path='.',genome=NULL,states=NULL,files=NULL,para=T) {
       buildStates <- FALSE
     }
   }
-  if(length(list.files(patt='webpage')) > 0) {
-    calls <- system2('grep', c('command', list.files(patt='webpage')), stdout=T)
-    argv <- parseCommand(calls)
-    genome <- argv$genome
-  }
   emis <- NULL
   trans <- NULL
-
-  modelFile <- grep('^model.*?\\.txt$', list.files(), value=T)
-  if(length(modelFile) > 0) {
-    model <- importModel(modelFile, loud=TRUE)
-    trans <- model$transitions
-    emis <- model$emissions
-    ## detect states from model, if possible and none provided
-    if(is.null(states) && buildStates) states <- model$states
-  } else {
-    message('No model file found, omitting shared model parameters')
-  } 
-
-  if(is.null(names(files))) names(files) <- files
   if(para == TRUE) {
     require(parallel)
     segs <- mclapply(files, importSegmentation, 
