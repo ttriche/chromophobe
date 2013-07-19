f998e4c269253c5b68f8584da6a5ce0e0ae47716
Tim Triche, Jr <ttriche@usc.edu>
2013-07-19 09:31:48 -0700

    a few minor changes before prerelease

--- R/writeLabelStates.R
+++ R/writeLabelStates.R
@@ -1,8 +1,8 @@
-writeLabelStates <- function(states, file='labelStates.txt', colors=NULL) {
+writeLabelStates <- function(obj, file='labelStates.txt', colors=NULL) {
 
   ## This function is a hack so that ChromHMM 1.0.5 will relabel states nicely.
   ## Nevertheless, it's a nice hack, especially when you're making slides :-)
-  labelStates <- as.data.frame(States)
+  labelStates <- as.data.frame(states(obj))
   if(!is.null(colors) && (!'color' %in% tolower(names(labelStates)))) {
     if(length(colors) == nrow(labelStates)) labelStates$Color <- colors
   }
