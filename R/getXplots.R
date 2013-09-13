6b378108b9ac5030ac7c9837d7ade4394c4e203c
Tim Triche, Jr <ttriche@usc.edu>
2013-09-13 12:49:00 -0700

    fix issue #2 for getXplots

--- R/getXplots.R
+++ R/getXplots.R
@@ -2,18 +2,24 @@ getXplots<-function(x, nFeats=500, col.fun='jet', clinical=NULL){
 
   ## FIXME: get rid of dependence on GMD
   require('GMD')
+  require('matrixStats')
   require('fastcluster')
   require('GenomicRanges')
   if(!is(x, 'SummarizedExperiment')) {
     stop('You need to provide a SummarizedExperiment for this to work')
+  } else if( !('chrX' %in% runValue(seqnames(x))) ){
+    stop('Your SummarizedExperiment does not contain any chrX probes!')
   }
   if( !('gender' %in% names(colData(x))) && !is.null(clinical) ){ 
     colData(x)[['gender']] <- clinical$gender
   }
-  Xloci <- sample(names(split(rowData(x),seqnames(rowData(x)))$chrX), nFeats)
-  XInd <- which( rownames(x) %in% grep('^cg', Xloci, val=T) )
 
-  ## FIXME: when CN data is available (as with hm450 chips), that's preferable
+  ## order by SD, then select top nFeats CpG loci 
+  x.chrX.SD <- rowSds(assays(keepSeqlevels(x, 'chrX'))[[1]])
+  Xloci <- names(head(sort(x.chrX.SD, decreasing=TRUE), nFeats))
+  XInd <- which(rownames(x) %in% grep('^cg', Xloci, val=T))
+
+  ## FIXME: when CN data is available (e.g. hm450 chips), that may be preferable
   ## FIXME: assign gender based on mean methylation (or whatever the assay is)
   if(!is.null(colData(x)$gender)) {
     colSide <- ifelse(is.na(colData(x)$gender), 'white',
@@ -24,7 +30,7 @@ getXplots<-function(x, nFeats=500, col.fun='jet', clinical=NULL){
   }
   name <- as.character(match.call()["x"])
   asy <- names(assays(x, withDimnames=F))[[1]]
-	tmp <- assays(x, withDimnames=F)[[asy]][XInd, ]
+  tmp <- assays(x, withDimnames=F)[[asy]][XInd, ]
   jet <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", 
                             "yellow", "#FF7F00", "red", "#7F0000"))
   bw <- colorRampPalette(c('white','gray','black'))
