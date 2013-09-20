e50facc81676fce320af00c034607e95086970bb
Tim Triche, Jr <ttriche@usc.edu>
2013-09-20 14:00:27 -0700

    fix issue #5, all subjects same sex (... usually)

--- R/getXplots.R
+++ R/getXplots.R
@@ -1,4 +1,4 @@
-getXplots<-function(x, nFeats=500, col.fun='jet', clinical=NULL){
+getXplots<-function(x, nFeats=500, col.fun='jet', clinical=NULL, pval=.000001){
 
   ## FIXME: get rid of dependence on GMD
   require('GMD')
@@ -20,7 +20,6 @@ getXplots<-function(x, nFeats=500, col.fun='jet', clinical=NULL){
   XInd <- which(rownames(x) %in% grep('^cg', Xloci, val=T))
 
   ## FIXME: when CN data is available (e.g. hm450 chips), that may be preferable
-  ## FIXME: assign gender based on mean methylation (or whatever the assay is)
   if(!is.null(colData(x)$gender)) {
     colSide <- ifelse(is.na(colData(x)$gender), 'white',
                       ifelse(substr(toupper(colData(x)$gender),1,1) == 'M', 
@@ -34,21 +33,37 @@ getXplots<-function(x, nFeats=500, col.fun='jet', clinical=NULL){
   jet <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", 
                             "yellow", "#FF7F00", "red", "#7F0000"))
   bw <- colorRampPalette(c('white','gray','black'))
-	capture.output({
-    clusts <- suppressWarnings(
-                heatmap.3(tmp, scale="none", trace="none", 
-                          ColIndividualColors=colSide, color.FUN=get(col.fun), 
-                          dendrogram='none', kc=2, labCol=colnames(x), 
-                          labRow=Xloci, Colv=T, Rowv=TRUE,
-                          main=paste('chrX clustering for', name)))
+  capture.output({
+        clusts <- suppressWarnings(
+                    heatmap.3(tmp, scale="none", trace="none", 
+                              ColIndividualColors=colSide, 
+                              color.FUN=get(col.fun), 
+                              dendrogram='none', kc=2, labCol=colnames(x), 
+                              labRow=Xloci, Colv=T, Rowv=TRUE,
+                              main=paste('chrX clustering for', name)))
   })
 
-  ## higher == female (XX with one Xi) 
-  means <- tapply(colMeans(tmp, na.rm=T), clusts$col.clusters, mean)
-  labels <- c('M','F')
-  if(means[1] > means[2]) labels <- rev(labels)
-  sex <- clusts$col.clusters
-  sex[clusts$colInd] <- sex
-  message('Assigned chrX cluster (assuming this is DNA methylation data):')
-  return(gsub('1', labels[1], gsub('2', labels[2], sex)))
+  by.clust <- lapply(unique(clusts$col.clusters), 
+                     function(x) {
+                       cm <- colMeans(tmp[, clusts$colInd], na.rm=TRUE)
+                       cm[clusts$col.clusters == x]
+                     })
+
+  ## do a test to quantify the chance they're all the same
+  p <- wilcox.test(by.clust[[1]], by.clust[[2]])$p.value
+  if(p > pval) {
+    message('Cannot distinguish two sexes.  Assigning everyone NA.')
+    return(rep(NA, length(clusts$col.clusters)))
+  } else { 
+    message('Pr(all samples are the same sex) = ', p)
+    labels <- c('M','F')
+    ## I feel dirty about this
+    sex <- clusts$col.clusters
+    sex[clusts$colInd] <- sex
+    ## higher == female (XX with one Xi) 
+    means <- do.call(c, lapply(by.clust, mean, na.rm=TRUE))
+    if(means[1] > means[2]) labels <- rev(labels)
+    message('Assigned chrX cluster (assuming this is DNA methylation data):')
+    return(gsub('1', labels[1], gsub('2', labels[2], sex)))
+  }
 }
