dda0221301cb6cfb6e7919005371d3b71fffa84d
Tim Triche, Jr <tim.triche@gmail.com>
2014-03-26 18:48:40 -0700

    tidy up plotAsGviz a little

--- R/plotAsGviz.R
+++ R/plotAsGviz.R
@@ -2,18 +2,21 @@ asGvizTracks <- function(HMM, ranges, ...) {
   cols <- as.list(stateColors(HMM))
   tracks <- lapply(segmentations(HMM), 
                    function(x) {
-                     xx <- subsetByOverlaps(x, ranges)
+                     xx <- sort(subsetByOverlaps(x, ranges))
+                     ## truncate so that all segments get plotted properly
+                     start(xx[1]) <- start(sort(ranges)[1])
+                     end(xx[length(xx)]) <- end(sort(ranges)[length(ranges)])
                      track <- AnnotationTrack(xx,
                                               stacking="dense",
                                               from=start(ranges),
                                               to=end(ranges),
                                               id=xx$state,
                                               feature=xx$state,
-                                              background.title='orange')
-                     displayPars(track) <- cols
+                                              background.title='darkred')
+                     displayPars(track) <- append(cols, list(lwd.border=0))
                      return(track)
                    })
-  for(i in names(tracks)) tracks[[i]]@name <- i
+  for(i in names(tracks)) tracks[[i]]@name <- paste0(i, ' HMM')
   return(tracks)
 }
 
